d2b732f55e4b344cf8b23450671cd4682027b9ac
mikestaar <mhykhelofficial@gmail.com>
2022-07-28 07:23:29 +0100

    fixed a bug preventing the app from working

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
 //get important DOM elements and store as variables
 
-const newTask = document.querySelector(".add-task-input");
-const submitTask = document.querySelector(".add-task-form");
-const filterTasks = document.querySelector(".filter-tasks");
-const taskList = document.querySelector(".task-list");
-const clearBtn = document.querySelector(".clear-all-button");
+const newTask = document.querySelector('.add-task-input');
+const submitTask = document.querySelector('.add-task-form');
+const filterTasks = document.querySelector('.filter-tasks');
+const taskList = document.querySelector('.task-list');
+const clearBtn = document.querySelector('.clear-all-button');
 
 //add all event listeners as a function
 
 function callAllEvents() {
+
   //call get input, create li and apppend function
-  submitTask.addEventListener("submit", addNewTask);
+  submitTask.addEventListener('submit', addNewTask);
 
   //call remove task event listener
-  taskList.addEventListener("click", removeTask);
+  taskList.addEventListener('click', removeTask);
 
   //call filter function
-  filterTasks.addEventListener("input", showFilterResults);
+  filterTasks.addEventListener('input', showFilterResults);
 
   //call clear button function
-  clearBtn.addEventListener("click", clearAllTasks);
+  clearBtn.addEventListener('click', clearAllTasks);
 
   //call reload event
-  document.addEventListener("DOMContentLoaded", collectAndDisplay);
+  document.addEventListener('DOMContentLoaded', collectAndDisplay);
 }
 
 //call event listener function
@@ -34,36 +35,36 @@ callAllEvents();
 function addNewTask(e) {
   e.preventDefault();
 
-  const isEmpty = (str) => !str.trim().length; //check for empty spaces
+  const isEmpty = str => !str.trim().length; //check for empty spaces
 
-  if (newTask.value === "") {
-    alert("Please input a task");
-  } else if (isEmpty(newTask.value)) {
+  if (newTask.value === '') {
+    alert('Please input a task');
+  } else if(isEmpty(newTask.value)){
     //alert the user if an empty space is entered as the "newTask value"
-    alert("Please input a task");
+    alert('Please input a task' );       
   } else {
     //declare new list item variable
-    let li = document.createElement("li");
+    let li = document.createElement('li');
     //add a class
-    li.className = "task-item";
+    li.className = 'task-item';
     //create new checkbox element
-    const checkBox = document.createElement("input");
+    const checkBox = document.createElement('input');
     //add type to checkbox
-    checkBox.type = "checkbox";
+    checkBox.type = 'checkbox';
     //add class to checkbox
-    checkBox.className = "ticker";
+    checkBox.className = 'ticker';
     //create link element
-    const rmIcon = document.createElement("span");
+    const rmIcon = document.createElement('span');
     //add class to link
-    rmIcon.className = "clear-icon";
+    rmIcon.className = 'clear-icon'
     //add content of span
-    rmIcon.innerHTML = '<a><i class="fas fa-trash-alt"></i></a>';
+    rmIcon.innerHTML = '<a><i class="fas fa-trash-alt"></i></a>'
     //append checkbox to li
     li.appendChild(checkBox);
     //append text to li
     li.appendChild(document.createTextNode(`${newTask.value}`));
     //append link to li
-    li.appendChild(rmIcon);
+    li.appendChild(rmIcon)
     //append li to main list
     taskList.appendChild(li);
 
@@ -71,136 +72,153 @@ function addNewTask(e) {
     updateLocalStorage(newTask.value);
 
     //add event listener for task complete
-    checkBox.addEventListener("change", taskComplete);
+    checkBox.addEventListener('change', taskComplete);
 
     //clear input
-    newTask.value = "";
-  }
-}
+    newTask.value = '';
+
+  };
+
+};
 
 //get all li elements and store them into local storage
 
 function updateLocalStorage(newData) {
-  let storage = localStorage.getItem("storedData");
+
+  let storage = localStorage.getItem('storedData');
 
   let dataToStore;
 
   if (storage === null) {
     dataToStore = [];
   } else {
-    dataToStore = JSON.parse(localStorage.getItem("storedData"));
-  }
+    dataToStore = JSON.parse(localStorage.getItem('storedData'))
+  };
 
   dataToStore.push(newData);
 
-  localStorage.setItem("storedData", JSON.stringify(dataToStore));
-}
+  localStorage.setItem('storedData', JSON.stringify(dataToStore));
+
+};
+
 
 //make the item crossed out on checkbox click
 
 function taskComplete(e) {
+
   if (e.target.checked) {
-    e.target.parentElement.style.textDecoration = "line-through";
-    e.target.parentElement.style.color = "grey";
+    e.target.parentElement.style.textDecoration = 'line-through';
+    e.target.parentElement.style.color = 'grey';
   } else {
-    e.target.parentElement.style.textDecoration = "none";
-    e.target.parentElement.style.color = "black";
-  }
-}
+    e.target.parentElement.style.textDecoration = 'none';
+    e.target.parentElement.style.color = 'black';
+  };
+
+};
+
 
 //remove item when delect icon is clicked
 
 function removeTask(e) {
-  if (e.target.parentElement.parentElement.classList.contains("clear-icon")) {
+
+  if (e.target.parentElement.parentElement.classList.contains('clear-icon')) {
     e.target.parentElement.parentElement.parentElement.remove();
 
     //call remove from local storage too
     removeFromLs(e.target.parentElement.parentElement.parentElement.innerText);
-  }
+  };
+
 }
 
 //also remove item from local storage
 
 function removeFromLs(whatToRemove) {
+
   //get local storage data
-  let dataFromStorage = JSON.parse(localStorage.getItem("storedData"));
+  let dataFromStorage = JSON.parse(localStorage.getItem('storedData'));
 
   //loop through the data and remove item
   dataFromStorage.forEach(function (data, index) {
+
     if (whatToRemove === data) {
       dataFromStorage.splice(index, 1);
-    }
+    };
+
   });
 
   //set local storage again with new array
-  localStorage.setItem("storedData", JSON.stringify(dataFromStorage));
-}
+  localStorage.setItem('storedData', JSON.stringify(dataFromStorage));
+
+};
 
 //set filter function
 
 function showFilterResults(e) {
-  const listItem = taskList.querySelectorAll("li");
+  const listItem = taskList.querySelectorAll('li');
   const filterText = e.target.value.toLowerCase();
 
   listItem.forEach(function (item) {
     if (item.innerText.toLowerCase().indexOf(filterText) !== -1) {
-      item.style.display = "block";
+      item.style.display = 'block';
     } else {
-      item.style.display = "none";
-    }
+      item.style.display = 'none';
+    };
   });
-}
+};
 
 //set function to remove all tasks when clear btn is clicked
 
 function clearAllTasks() {
+
   if (taskList.firstElementChild) {
-    if (
-      confirm("This will erase all tasks, are you sure you want to proceed?")
-    ) {
+    if (confirm('This will erase all tasks, are you sure you want to proceed?')) {
+
       while (taskList.firstElementChild) {
         taskList.firstElementChild.remove();
-      }
-    }
+      };
+    };
 
     //also clear local storage
-    localStorage.removeItem("storedData");
-  }
-}
+    localStorage.removeItem('storedData');
+  };
 
-//funtion to get all LS data on reload and display them in
+};
+
+
+//funtion to get all LS data on reload and display them in 
 
 function collectAndDisplay() {
-  const data = JSON.parse(localStorage.getItem("storedData"));
+
+  const data = JSON.parse(localStorage.getItem('storedData'));
 
   if (data !== null) {
     data.forEach(function (item) {
       //declare new list item variable
-      let li = document.createElement("li");
+      let li = document.createElement('li');
       //add a class
-      li.className = "task-item";
+      li.className = 'task-item';
       //create new checkbox element
-      const checkBox = document.createElement("input");
+      const checkBox = document.createElement('input');
       //add type to checkbox
-      checkBox.type = "checkbox";
+      checkBox.type = 'checkbox';
       //add class to checkbox
-      checkBox.className = "ticker";
+      checkBox.className = 'ticker';
       //create link element
-      const rmIcon = document.createElement("span");
+      const rmIcon = document.createElement('span');
       //add class to link
-      rmIcon.className = "clear-icon";
+      rmIcon.className = 'clear-icon'
       //add content of span
-      rmIcon.innerHTML = '<a><i class="fas fa-trash-alt"></i></a>';
+      rmIcon.innerHTML = '<a><i class="fas fa-trash-alt"></i></a>'
       //append checkbox to li
       li.appendChild(checkBox);
       //append text to li
       li.appendChild(document.createTextNode(item));
       //append link to li
-      li.appendChild(rmIcon);
+      li.appendChild(rmIcon)
       //append li to main list
       taskList.appendChild(li);
       //add event listener for task complete
-      checkBox.addEventListener("change", taskComplete);
+      checkBox.addEventListener('change', taskComplete);
     });
   }
-}
+};
